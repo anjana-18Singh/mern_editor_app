f0b5aaa5d3ed30cea1e42ff7ac43eca74e6982ac
Anjana-18Singh <121855141+anjana-18Singh@users.noreply.github.com>
2025-01-20 13:55:37 +0530

    Update Create.jsx

--- frontend/src/components/Create.jsx
+++ frontend/src/components/Create.jsx
@@ -18,7 +18,7 @@ const Create = () => {
 
         const addUser={name,email,age};
         try{
-        const response=await fetch("http://localhost:8000",{
+        const response=await fetch("https://mern-editor-app-backend.onrender.com",{
             method:'POST',
             body: JSON.stringify(addUser),
             headers:{
